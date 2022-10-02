4063ddadae7e07539becb78f932cf19b4af3df75
gary003 <gary.johnson003@gmail.com>
2022-10-02 19:07:57 +0200

    init commit

---
+++ src/serverTodoTR.ts
@@ -0,0 +1,49 @@
+import express from "express"
+import * as http from "http"
+import { Server } from "socket.io"
+import * as lib_ip from "ip"
+import path from "path"
+
+const port_ecoute: number = 8088
+const todoList: string[] = []
+
+const app = express()
+const server = http.createServer(app)
+
+const pathToView: string = path.join("__dirname", "..", "views")
+
+app.set("views", pathToView)
+app.get("/", function (req, res) {
+  const ip_port: string = lib_ip.address() + ":" + port_ecoute.toString()
+  res.render("todo.ejs", { mon_ip_port: ip_port })
+  res.end()
+})
+
+const io = new Server(server)
+io.sockets.on("connection", function (la_socket) {
+  la_socket.emit("connected", todoList)
+
+  la_socket.on("add", function (mess: string) {
+    const clean_message: string = mess.trim()
+
+    la_socket.broadcast.emit("add", clean_message)
+    la_socket.emit("add", clean_message)
+    todoList.push(clean_message)
+  })
+
+  la_socket.on("remove", function (mess: string) {
+    const clean_message: string = mess.trim()
+
+    if (todoList.indexOf(clean_message) > -1) {
+      la_socket.broadcast.emit("remove", clean_message)
+      la_socket.emit("remove", clean_message)
+      todoList.splice(todoList.indexOf(clean_message), 1)
+    }
+  })
+})
+
+server.listen(port_ecoute, function () {
+  console.log("adresse serveur : " + lib_ip.address())
+  console.log("ecoute sur le port : " + port_ecoute)
+  console.log("lien url " + lib_ip.address() + ":" + port_ecoute)
+})
